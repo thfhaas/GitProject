c3574b181b8a9b10bdfa73180cf5dbb201119608
ThomasHaas <thomas.haas@kuleuven.be>
2018-06-14 11:51:20 +0200

    Initial commit

---
+++ functions/arithmetic_functions.py
@@ -0,0 +1,11 @@
+def addition(x,y):
+    pass
+
+def subtraction(x,y):
+    pass
+
+def multiplication(x,y):
+    pass
+
+def division(x,y):
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import functions.arithmetic_functions as afun
+
+x = 2.0
+y = 5.0
+
+z = afun.addition(x,y)
+
+print(z)
