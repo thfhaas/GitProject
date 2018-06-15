cfcbe5929eb0c6f8b3feca820af3e6f3f2fe45c1
thfhaas <thf.haas@gmail.com>
2018-06-15 09:57:16 +0200

    Polynomial function

--- functions/polynomial_functions.py
+++ functions/polynomial_functions.py
@@ -1,11 +1,2 @@
-def addition(x,y):
-    return x+y
-
-def subtraction(x,y):
-    return x-y
-
-def multiplication(x,y):
-    return x*y
-
-def division(x,y):
-    return x/y
+def poly(x,p):
+    return x**p
