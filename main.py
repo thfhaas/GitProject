aac98fabd340c78cee7a8cbc70c748f76376c620
ThomasHaas <thomas.haas@kuleuven.be>
2018-06-19 11:58:30 +0200

    change to main

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import numpy as np
+
 import functions.arithmetic_functions as afun
+import functions.polynomial_functions as pfun
 
 x = 2.0
-y = 5.0
+y = 3.0
 
 z = afun.addition(x,y)
+print(z)
+
+z = pfun.poly(x,y)
+print(z)
+
+z = np.zeros(10)
+for k in range(10):
+    z[k] = pfun.poly(x,k)
 
 print(z)
